ee61c5a20f10b6c9df92384c669fb7eda404884d
srikanthcs7 <97518385+srikanthcs7@users.noreply.github.com>
2025-01-11 19:08:36 +0530

    fix: remove unused

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -23,26 +17,5 @@ const nextConfig = {
   },
 }
 
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
 
 export default nextConfig
