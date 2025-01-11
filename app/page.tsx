99c9e9bb4c33eeed1eb2079a31491a8a2c656cc6
srikanthcs7 <97518385+srikanthcs7@users.noreply.github.com>
2025-01-11 19:32:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
                 role: 'Frontend Engineer - SDE',
                 date: 'July 2021 - July 2024',
                 points: [
-                  'Successfully migrated an outdated B2B dashboard to the platform, leading to increased revenue from‬ expanded customer offerings and significant cost savings.‬',
+                  'Migrated an outdated B2B dashboard, boosting revenue with expanded offerings and achieving major cost savings.‬',
                   'Optimized multiple sets of pages to enhance rendering performance',
                   'Integrated the Ridecell plugin into the Geotab platform',
                   'Implemented Cypress as an end-to-end test tool across dashboards'
